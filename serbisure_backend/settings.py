9fa818fa595c6903f67e4cfb1857e654391484bf
rhoii <rhoydelm@gmail.com>
2026-05-01 23:47:29 +0800

    AppDev: DRF backend for system implemented

--- serbisure_backend/settings.py
+++ serbisure_backend/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = env('SECRET_KEY', default='django-insecure-default-change-me-in-pro
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = ['*']
 
 # Support for Vercel/Render wildcard subdomains
 if os.environ.get('VERCEL_URL'):
